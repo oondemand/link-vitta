f00ebd295be31265700aa2485f8364b0add40d0c
Maike <maikonalexandre574@gmail.com>
2025-04-11 10:23:08 -0300

    fix: update status response to use a green circle emoji for consistency

--- src/routes/status.js
+++ src/routes/status.js
@@ -4,8 +4,7 @@ import { env } from "../config/env.js";
 const statusRoutes = express.Router();
 
 statusRoutes.get("/", async (req, res) => {
-  res.json({ status: "🟩 OK", message: env.SERVICE_VERSION });
+  res.json({ status: "🟢 OK", message: env.SERVICE_VERSION });
 });
 
 export { statusRoutes };
-
